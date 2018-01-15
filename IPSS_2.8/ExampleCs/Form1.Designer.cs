61f4658e33ba67186454fac782e8991b86a57628
vohungvi <vohungvi@vohungvi.com>
2018-01-15 22:50:59 +0700

    + add Example cs

---
+++ IPSS_2.8/ExampleCs/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 302);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 355);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IPSS demo - 0939.825.125";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ IPSS_2.8/ExampleCs/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IPSS;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //khởi tạo đối tượng
+            IPSSbike detector = new IPSSbike();
+
+            //CÁC OPTION
+
+            //đường dẫn chứa file output, nếu set null hoặc rỗng thì sẽ không save ảnh output
+            detector.OutputFoler = @"D:\Result";
+                        
+
+            //engine nhận tham số là ảnh hoặc đường dẫn đến ảnh
+            BikePlate result = detector.ReadPlate(@"8892.jpg");
+
+            //kết quả trả về là class IPSSresult gồm nhiều thuộc tính
+            //text là các ký tự biển số (phải có key mới trả về kết quả)
+            label1.Text = result.text;
+
+            //bitmap là hình ảnh kết quả
+            Bitmap bmp = result.bitmap;
+            pictureBox1.Image = bmp;
+
+            //thời gian xử lý
+            int ms = result.elapsedMilisecond;
+
+            //mã lỗi (nếu có)
+            string error = result.error;
+
+            //nếu có biển số giá trị là true
+            bool hasPlate = result.hasPlate;
+
+            //nếu đọc đủ các ký tự thì kết quả là true
+            bool isValid = result.isValid;            
+        }
+    }
+}
