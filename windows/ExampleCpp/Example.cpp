4dbe256d3a495af71662e1db06161c9879450ecc
vohungvi <vohungvi27@gmail.com>
2020-05-14 11:57:34 +0700

    + add text

--- windows/ExampleCpp/Example.cpp
+++ windows/ExampleCpp/Example.cpp
@@ -10,11 +10,34 @@ using namespace IPSS;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//Init class
 	IPSSbike::Init();
+
+	//set write log to txt file in same dir with dll file
 	IPSSbike::SetEnableLog(false);
 
-
+	//you can set input pameter is image path
 	BikePlate result = IPSSbike::ReadPlate("8892.jpg");
+
+
+	//result is a class contain some values
+	//.text is plate character
+	std::cout<< result.text << "\n";
+
+
+	//elapsed time read plate
+	int ms = result.elapsedMilisecond;
+
+
+	//error code to diagnostic
+	std::cout << result.error << "\n";
+
+
+	//.hasPlate is true when found plate in image
+	bool hasPlate = result.hasPlate;
+
+	//.isValid is true when read enough character
+	bool isValid = result.isValid;
 
 
 	std::cout << result.text;
--- windows/ExampleCs/Form1.cs
+++ windows/ExampleCs/Form1.cs
@@ -20,36 +20,45 @@ namespace Example
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //khởi tạo đối tượng
+            //Init class
             IPSSbike detector = new IPSSbike();
 
-            //CÁC OPTION
+            //OPTIONs
 
             //đường dẫn chứa file output, nếu set null hoặc rỗng thì sẽ không save ảnh output
+            //set output dir, if output dir is null or empty engine will not save output image
             detector.OutputFoler = @"D:\Result";
                         
 
             //engine nhận tham số là ảnh hoặc đường dẫn đến ảnh
+            //you can set input pameter is bitmap or image path
             BikePlate result = detector.ReadPlate(@"8892.jpg");
 
             //kết quả trả về là class IPSSresult gồm nhiều thuộc tính
-            //text là các ký tự biển số (phải có key mới trả về kết quả)
+            //.text là các ký tự biển số (phải có key mới trả về kết quả)
+            //result is a class contain some values
+            //.text is plate character
             label1.Text = result.text;
 
             //bitmap là hình ảnh kết quả
+            //bimap is image has returned
             Bitmap bmp = result.bitmap;
             pictureBox1.Image = bmp;
 
             //thời gian xử lý
+            //elapsed time read plate
             int ms = result.elapsedMilisecond;
 
             //mã lỗi (nếu có)
+            //error code to diagnostic
             string error = result.error;
 
             //nếu có biển số giá trị là true
+            //.hasPlate is true when found plate in image
             bool hasPlate = result.hasPlate;
 
             //nếu đọc đủ các ký tự thì kết quả là true
+            //.isValid is true when read enough character
             bool isValid = result.isValid;            
         }
     }
